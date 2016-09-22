4d43ee476b91f92d868c8ea8dd42d307a4a0cce0
Chris Liu <lyc19970919@gmail.com>
2016-09-22 15:07:01 -0300

    Add files via upload

---
+++ YuchenLiu_Ex4/sketch.js
@@ -0,0 +1,145 @@
+function setup(){
+  createCanvas(185, 212);
+  background(255);
+  fill(222, 207, 180, 80);
+  rect(0, 0, 185, 212);
+  
+  arc(25.5, 119.5, 20, 20, PI + QUARTER_PI, PI + HALF_PI + QUARTER_PI, OPEN);
+  noFill();
+  strokeWeight(3);
+  stroke(79, 177, 211, 150);
+  arc(25.5, 119.5, 28, 28, PI + QUARTER_PI, PI + HALF_PI + QUARTER_PI, OPEN);
+  stroke(237, 203, 110, 180);
+  arc(27.5, 119.5, 40, 46, PI + QUARTER_PI, PI + HALF_PI + QUARTER_PI, OPEN);
+  
+  strokeWeight(1);
+  stroke(0);
+  fill(255, 200);
+  ellipse(110, 8, 52, 52);
+  
+  strokeWeight(4.5);
+  ellipse(110, 8, 36, 36);
+  
+  strokeWeight(1);
+  stroke(222, 92, 94, 134);
+  ellipse(110, 8, 31, 31);
+  
+  stroke(0);
+  fill(0, 200);
+  triangle(0, 94, 0, 181, 41, 134);
+  
+  fill(203, 197, 173);
+  quad(0, 115, 0, 127, 8, 137, 14, 129);
+  quad(8, 137, 16, 144, 8, 153, 1, 145);
+  quad(8, 153, 17, 161, 11, 168, 1, 161);
+  quad(16, 144, 23, 135, 32, 144, 25, 151);
+  fill(255, 250);
+  quad(14, 129, 25.5, 119.5, 34.5, 127.5, 25, 138);
+  fill(157, 104, 24);
+  quad(24, 137, 34.5, 127.5, 40, 134, 31, 144);
+  fill(22, 90, 14);
+  triangle(0, 145, 8, 153, 0, 161);
+  fill(250, 182, 210, 240);
+  triangle(0, 161, 0, 181, 11, 168);
+  
+  line(0, 181, 127, 39);
+  line(25, 138, 122, 35);
+  line(127, 39, 122, 35);
+  line(25.5, 119.5, 73, 74);
+  
+  fill(0);
+  quad(34.5, 127.5, 122, 35, 127, 39, 40, 134);
+  fill(203, 197, 173);
+  quad(34.5, 127.5, 40, 134, 59, 115, 53, 108);
+  quad(69, 90, 79, 79, 85, 86, 75, 97);
+  quad(90, 68, 98, 59, 104, 65, 95, 75);
+  quad(109, 49, 115, 41, 120, 46, 114, 54);
+  
+  line(64, 85, 69, 90);
+  quad(25.5, 119.5, 34.5, 127.5, 69, 90, 63.5, 84);
+  line(47, 101, 59, 115);
+  fill(0);
+  quad(69, 90, 63.5, 84, 73, 74, 79, 79);
+  
+  strokeWeight(5);
+  line(0, 172, 212, 139);
+  
+  strokeWeight(3);
+  fill(255);
+  stroke(150, 150, 150);
+  ellipse(81, 212, 64, 64);
+  stroke(213, 220, 24);
+  ellipse(149, 212, 72, 72);
+  strokeWeight(0.5);
+  stroke(0);
+  ellipse(81, 212, 64, 64);
+  ellipse(149, 212, 72, 72);
+  
+  strokeWeight(2);
+  stroke(0);
+  line(0, 210, 185, 212);
+  
+  stroke(165, 64, 38);
+  line(115, 70, 185, 35);
+  
+  strokeWeight(1);
+  stroke(0);
+  line(123, 50, 185, 15);
+  strokeWeight(0.5);
+  line(123, 47, 185, 12);
+  
+  strokeWeight(2);
+  stroke(0);
+  line(85, 212, 148, 0);
+  
+  strokeWeight(4);
+  stroke(88, 45, 34);
+  line(0, 198, 172, 153);
+  
+  strokeWeight(1);
+  stroke(0);
+  line(0, 0, 185, 162);
+  
+  line(0, 50, 82, 33);
+  
+  noFill();
+  arc(70, 144, 250, 230, PI, 4.9, OPEN);
+  
+  line(50, 0, 46, 15);
+  
+  strokeWeight(2.5);
+  stroke(203, 195, 193, 150);
+  line(42, 0, 44, 10);
+  
+  strokeWeight(1);
+  stroke(0);
+  fill(0);
+  triangle(185, 0, 185, 4, 100, 115);
+  
+  noStroke();
+  fill(64, 82, 25);
+  triangle(185, 4, 185, 10, 150, 45);
+  
+  strokeWeight(2);
+  noFill();
+  stroke(165, 64, 38);
+  arc(185, 75, 40, 40, 2.5, PI + PI, OPEN);
+  strokeWeight(1);
+  stroke(0);
+  arc(185, 75, 37.5, 37.5, 2.5, PI + PI, OPEN);
+  
+  line(185, 70, 165, 86);
+  
+  noStroke();
+  fill(247, 180, 223, 50);
+  quad(148, 0, 185, 0, 185, 162, 117, 102); 
+  
+  strokeWeight(0.5);
+  fill(108, 92, 8, 200);
+  triangle(185, 110, 185, 142, 137, 143); 
+  
+ 
+ 
+  
+  
+}
